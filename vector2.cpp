dbeef836acdacbbd67c7d864d47a8e209461ac04
Anemosian <Anemosian@users.noreply.github.com>
2016-02-04 09:57:37 +0800

    Added postfix maker via input.
    Input still needs work. What am I doing?

--- vector2.cpp
+++ vector2.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
+#include<cstring>
+#include<stack>
 
+using namespace std;
 
 template<class T>
 class vector2
 {
 	public:
-	string name;
 	T x;
 	T y;
 	
-
+	//constructor
+	vector2::vector2(string& name, T& a, T&b)
+	{
+		vector2 name;
+		name.x = a;
+		name.y = b;
+	}
+	
 	//vector addition
 	vector2& operator+(const vector2& v)
 	{
@@ -53,9 +61,98 @@ class vector2
 	}
 };
 
+int priority(char ch) 
+{
+   switch (ch) 
+   {
+      case '*': return 4;
+	  case '/': return 3;
+      case '+': return 2;
+      case '-': return 1;
+      default : return 0;
+   }
+}
+
+void postfixer(char infix[], char postfix[], int size) {
+	stack<char> s;
+	int weight;
+	int i = 0;
+	int k = 0;
+	char ch;
+
+	while (i < size) 
+	{
+		ch = infix[i];
+		//for the parentheses
+		if (ch == '(') 
+		{
+			s.push(ch);
+			i++;
+			continue;
+		}
+		if (ch == ')') 
+		{
+			while (!s.empty() && s.top() != '(') 
+			{
+				postfix[k++] = s.top();
+				s.pop();
+			}
+			if (!s.empty()) 
+			{
+				s.pop();
+			}
+			i++;
+			continue;
+		}
+		//check if operator or number
+		weight = getWeight(ch);
+		if (weight == 0) 
+		{
+			postfix[k++] = ch;
+		}
+		else 
+		{
+			if (s.empty()) 
+			{
+				s.push(ch);
+			}
+			else 
+			{
+				while (!s.empty() && s.top() != '(' && weight <= getWeight(s.top())) 
+				{
+					postfix[k++] = s.top();
+					s.pop();
+				}
+				s.push(ch);
+			}
+		}
+		i++;
+	}
+	while (!s.empty()) 
+	{
+		postfix[k++] = s.top();
+		s.pop();
+	}
+	postfix[k] = 0;
+}
 
 int main(int argc, char** argv)
 {
+	string declaration;
+	string input;
+	while(input != '=')
+	{
+		cin >> input;
+		char infix[] = input;
+		int size = strlen(infix);
+		char postfix[size];
+	}
+   
+	int size = strlen(infix);
+	char postfix[size];
+	postifxer(infix,postfix,size);
+	
+	return 0;
 	
 	
 }
