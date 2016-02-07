2e5b8971cd0350f46378463b8f5767d5240a1bdb
erocrizs <aemielvin.loremia@gmail.com>
2016-02-08 01:03:22 +0800

    Add a token struct

--- vector_calc.cpp
+++ vector_calc.cpp
@@ -4,6 +4,29 @@
 #include <iostream>
 using namespace std;
 
+struct token {
+	union {
+		char op;
+		float f;
+		Vector2 v;
+	};
+	enum {
+		SCALAR,
+		VECTOR,
+		OPERATOR
+	} type;
+	
+	token(float f=0): type(SCALAR) {
+		this->f = f;
+	}
+	token(Vector2 v): type(VECTOR) {
+		this->v = v;
+	}
+	token(char op): type(OPERATOR) {
+		this->op = op;
+	}
+};
+
 int main()
 {
 	map<string, Vector2> variable;
